214bdd7e4a31250e2b6275e6ede83157fcbf824f
ysouyno <ysouyno@163.com>
2020-07-17 09:53:33 +0800

    chapter 1

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # fake_dns_server
+
+## `linux`发行版的选择
+
+参考自：“[Building a DNS server in Rust](https://github.com/EmilHernvall/dnsguide)”。
+
+安装`dig`和`nc`两个程序在`archlinux`上，但是我没有尝试成功，可能是`nc`的原因，它生成的`query_packet.txt`文件大小为`0`：
+
+``` shellsession
+% sudo pacman -S dnsutil
+% sudo pacman -S gnu-netcat
+```
+
+``` shellsession
+% nc -u -l 1053 > query_packet.txt
+```
+
+最后在`debian`尝试发现可行。
+
+## 文件`query_packet.txt`和`response_packet.txt`是哪里来的
+
+首先执行`nc`命令，`debian`下的`nc`命令参数与文章中描述的稍有不同，端口前使用`-p`参数，如下：
+
+``` shellsession
+$ nc -u -l -p 1053 > query_packet.txt
+```
+
+打开另一个`Terminal`执行：
+
+``` shellsession
+$ dig +retry=0 -p 1053 @127.0.0.1 +noedns baidu.com
+```
+
+在`dig`命令返回后`Ctrl+C`结束`nc`，此时生成`query_packet.txt`，然后运行以下命令生成`response_packet.txt`：
+
+``` shellsession
+nc -u 8.8.8.8 53 < query_packet.txt > response_packet.txt
+```
+
+执行一秒后即可用`Ctrl+C`结束`nc`，此时生成`response_packet.txt`。
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,411 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::Read;
+use std::net::Ipv4Addr;
+
+type Error = Box<dyn std::error::Error>;
+type Result<T> = std::result::Result<T, Error>;
+
+pub struct ByteBuffer {
+    pub buf: [u8; 512],
+    pub pos: usize,
+}
+
+impl ByteBuffer {
+    pub fn new() -> ByteBuffer {
+        ByteBuffer {
+            buf: [0; 512],
+            pos: 0,
+        }
+    }
+
+    /// Current position within buffer
+    fn pos(&self) -> usize {
+        self.pos
+    }
+
+    /// Step the buffer position forward a specific number of steps
+    fn step(&mut self, steps: usize) -> Result<()> {
+        self.pos += steps;
+
+        Ok(())
+    }
+
+    /// Change the buffer position
+    fn seek(&mut self, pos: usize) -> Result<()> {
+        self.pos = pos;
+
+        Ok(())
+    }
+
+    /// Read a single byte from `pos` and move the `pos` one step forward
+    fn read(&mut self) -> Result<u8> {
+        if self.pos >= 512 {
+            return Err("End of buffer".into());
+        }
+
+        let res = self.buf[self.pos];
+        self.pos += 1;
+
+        Ok(res)
+    }
+
+    /// Get a single byte, without changing `pos`
+    fn get(&self, pos: usize) -> Result<u8> {
+        if pos >= 512 {
+            return Err("End of buffer".into());
+        }
+
+        Ok(self.buf[pos])
+    }
+
+    /// Get a range of bytes
+    fn get_range(&self, start: usize, len: usize) -> Result<&[u8]> {
+        if start + len >= 512 {
+            return Err("End of buffer".into());
+        }
+
+        Ok(&self.buf[start..start + len])
+    }
+
+    /// Read 2 bytes from current `pos`, stepping 2 steps forward
+    fn read_u16(&mut self) -> Result<u16> {
+        let res = ((self.read()? as u16) << 8) | (self.read()? as u16);
+
+        Ok(res)
+    }
+
+    /// Read 4 bytes from current `pos`, stepping 4 steps forward
+    fn read_u32(&mut self) -> Result<u32> {
+        let res = ((self.read()? as u32) << 24)
+            | ((self.read()? as u32) << 16)
+            | ((self.read()? as u32) << 8)
+            | ((self.read()? as u32) << 0);
+
+        Ok(res)
+    }
+
+    /// Read a qname
+    fn read_qname(&mut self, outstr: &mut String) -> Result<()> {
+        let mut pos = self.pos();
+        let mut jumped = false;
+        let max_jumps = 5;
+        let mut jumps_performed = 0;
+        let mut delim = "";
+
+        loop {
+            if jumps_performed > max_jumps {
+                return Err(format!("Limit of {} jumps exceeded", max_jumps).into());
+            }
+
+            let len = self.get(pos)?;
+            if (len & 0xC0) == 0xC0 {
+                if !jumped {
+                    self.seek(pos + 2)?;
+                }
+
+                let b2 = self.get(pos + 1)? as u16;
+                let offset = (((len as u16) ^ 0xC0) << 8) | b2;
+                pos = offset as usize;
+
+                jumped = true;
+                jumps_performed += 1;
+
+                continue;
+            } else {
+                pos += 1;
+
+                if len == 0 {
+                    break;
+                }
+
+                outstr.push_str(delim);
+
+                let str_buffer = self.get_range(pos, len as usize)?;
+                outstr.push_str(&String::from_utf8_lossy(str_buffer).to_lowercase());
+
+                delim = ".";
+
+                pos += len as usize;
+            }
+        }
+
+        if !jumped {
+            self.seek(pos)?;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub enum ResultCode {
+    NOERROR = 0,
+    FORMERR = 1,
+    SERVFAIL = 2,
+    NXDOMAIN = 3,
+    NOTIMP = 4,
+    REFUSED = 5,
+}
+
+impl ResultCode {
+    pub fn from_num(num: u8) -> ResultCode {
+        match num {
+            1 => ResultCode::FORMERR,
+            2 => ResultCode::SERVFAIL,
+            3 => ResultCode::NXDOMAIN,
+            4 => ResultCode::NOTIMP,
+            5 => ResultCode::REFUSED,
+            0 | _ => ResultCode::NOERROR,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct DnsHeader {
+    // 2 bytes
+    pub id: u16,
+
+    // 1 byte
+    pub recursion_desired: bool,
+    pub truncated_message: bool,
+    pub authoritative_answer: bool,
+    pub opcode: u8,
+    pub response: bool,
+
+    // 1 byte
+    pub rescode: ResultCode,
+    pub checking_disabled: bool,
+    pub authed_data: bool,
+    pub z: bool,
+    pub recursion_available: bool,
+
+    // 8 bytes
+    pub questions: u16,
+    pub answers: u16,
+    pub authoritative_entries: u16,
+    pub resource_entries: u16,
+}
+
+impl DnsHeader {
+    pub fn new() -> DnsHeader {
+        DnsHeader {
+            id: 0,
+
+            recursion_desired: false,
+            truncated_message: false,
+            authoritative_answer: false,
+            opcode: 0,
+            response: false,
+
+            rescode: ResultCode::NOERROR,
+            checking_disabled: false,
+            authed_data: false,
+            z: false,
+            recursion_available: false,
+
+            questions: 0,
+            answers: 0,
+            authoritative_entries: 0,
+            resource_entries: 0,
+        }
+    }
+
+    pub fn read(&mut self, buffer: &mut ByteBuffer) -> Result<()> {
+        self.id = buffer.read_u16()?;
+
+        let flags = buffer.read_u16()?;
+        let a = (flags >> 8) as u8;
+        let b = (flags & 0xFF) as u8;
+
+        self.recursion_desired = (a & (1 << 0)) > 0;
+        self.truncated_message = (a & (1 << 1)) > 0;
+        self.authoritative_answer = (a & (1 << 2)) > 0;
+        self.opcode = (a >> 3) & 0x0F;
+        self.response = (a & (1 << 7)) > 0;
+
+        self.rescode = ResultCode::from_num(b & 0x0F);
+        self.checking_disabled = (b & (1 << 4)) > 0;
+        self.authed_data = (b & (1 << 5)) > 0;
+        self.z = (b & (1 << 6)) > 0;
+        self.recursion_available = (b & (1 << 7)) > 0;
+
+        self.questions = buffer.read_u16()?;
+        self.answers = buffer.read_u16()?;
+        self.authoritative_entries = buffer.read_u16()?;
+        self.resource_entries = buffer.read_u16()?;
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub enum QueryType {
+    UNKNOWN(u16),
+    A,
+}
+
+impl QueryType {
+    pub fn to_num(&self) -> u16 {
+        match *self {
+            QueryType::UNKNOWN(x) => x,
+            QueryType::A => 1,
+        }
+    }
+
+    pub fn from_num(num: u16) -> QueryType {
+        match num {
+            1 => QueryType::A,
+            _ => QueryType::UNKNOWN(num),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct DnsQuestion {
+    pub name: String,
+    pub qtype: QueryType,
+}
+
+impl DnsQuestion {
+    pub fn new(name: String, qtype: QueryType) -> DnsQuestion {
+        DnsQuestion { name, qtype }
+    }
+
+    pub fn read(&mut self, buffer: &mut ByteBuffer) -> Result<()> {
+        buffer.read_qname(&mut self.name)?;
+        self.qtype = QueryType::from_num(buffer.read_u16()?);
+        let _ = buffer.read_u16()?; // class
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub enum DnsRecord {
+    UNKNOWN {
+        domain: String,
+        qtype: u16,
+        data_len: u16,
+        ttl: u32,
+    },
+
+    A {
+        domain: String,
+        addr: Ipv4Addr,
+        ttl: u32,
+    },
+}
+
+impl DnsRecord {
+    pub fn read(buffer: &mut ByteBuffer) -> Result<DnsRecord> {
+        let mut domain = String::new();
+        buffer.read_qname(&mut domain)?;
+
+        let qtype_num = buffer.read_u16()?;
+        let qtype = QueryType::from_num(qtype_num);
+        let _ = buffer.read_u16()?;
+        let ttl = buffer.read_u32()?;
+        let data_len = buffer.read_u16()?;
+
+        match qtype {
+            QueryType::A => {
+                let raw_addr = buffer.read_u32()?;
+                let addr = Ipv4Addr::new(
+                    ((raw_addr >> 24) & 0xFF) as u8,
+                    ((raw_addr >> 16) & 0xFF) as u8,
+                    ((raw_addr >> 8) & 0xFF) as u8,
+                    ((raw_addr >> 0) & 0xFF) as u8,
+                );
+
+                Ok(DnsRecord::A { domain, addr, ttl })
+            }
+
+            QueryType::UNKNOWN(_) => {
+                buffer.step(data_len as usize)?;
+
+                Ok(DnsRecord::UNKNOWN {
+                    domain,
+                    qtype: qtype_num,
+                    data_len,
+                    ttl,
+                })
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct DnsPacket {
+    pub header: DnsHeader,
+    pub questions: Vec<DnsQuestion>,
+    pub answers: Vec<DnsRecord>,
+    pub authorities: Vec<DnsRecord>,
+    pub resources: Vec<DnsRecord>,
+}
+
+impl DnsPacket {
+    pub fn new() -> DnsPacket {
+        DnsPacket {
+            header: DnsHeader::new(),
+            questions: Vec::new(),
+            answers: Vec::new(),
+            authorities: Vec::new(),
+            resources: Vec::new(),
+        }
+    }
+
+    pub fn from_buffer(buffer: &mut ByteBuffer) -> Result<DnsPacket> {
+        let mut result = DnsPacket::new();
+        result.header.read(buffer)?;
+
+        for _ in 0..result.header.questions {
+            let mut question = DnsQuestion::new("".to_string(), QueryType::UNKNOWN(0));
+            question.read(buffer)?;
+            result.questions.push(question);
+        }
+
+        for _ in 0..result.header.answers {
+            let rec = DnsRecord::read(buffer)?;
+            result.answers.push(rec);
+        }
+
+        for _ in 0..result.header.authoritative_entries {
+            let rec = DnsRecord::read(buffer)?;
+            result.authorities.push(rec);
+        }
+
+        for _ in 0..result.header.resource_entries {
+            let rec = DnsRecord::read(buffer)?;
+            result.resources.push(rec);
+        }
+
+        Ok(result)
+    }
+}
+
+fn main() -> Result<()> {
+    let mut f = File::open("response_packet.txt")?;
+    let mut buffer = ByteBuffer::new();
+    f.read(&mut buffer.buf)?;
+
+    let packet = DnsPacket::from_buffer(&mut buffer)?;
+    println!("{:#?}", packet.header);
+
+    for q in packet.questions {
+        println!("{:#?}", q);
+    }
+
+    for rec in packet.answers {
+        println!("{:#?}", rec);
+    }
+
+    for rec in packet.authorities {
+        println!("{:#?}", rec);
+    }
+
+    for rec in packet.resources {
+        println!("{:#?}", rec);
+    }
+
+    Ok(())
 }
