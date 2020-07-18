4633f3a88824b12cbcb0bc161bea5ae2ae5e42ae
ysouyno <ysouyno@163.com>
2020-07-18 16:05:25 +0800

    Chapter 5

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # fake_dns_server
 
-## `linux`发行版的选择
+## `Linux`发行版的选择
 
 参考自：“[Building a DNS server in Rust](https://github.com/EmilHernvall/dnsguide)”。
 
@@ -41,7 +41,7 @@ $ nc -u 8.8.8.8 53 < query_packet.txt > response_packet.txt
 
 ## `dig` for Windows
 
-可以在这里找到：“[Dig command on Windows OS](https://websistent.com/dig-command-on-windows-os/)”，附“[dig-for-windows-9.9.5-W1.zip](files/dig-for-windows-9.9.5-W1.zip)”。
+可以在这里找到：“[Dig command on Windows OS](https://websistent.com/dig-command-on-windows-os/)”，附：“[dig-for-windows-9.9.5-W1.zip](files/dig-for-windows-9.9.5-W1.zip)”。
 
 ## Chapter 4
 
@@ -78,3 +78,78 @@ google.com.             60      IN      A       46.82.174.69
 Received query: DnsQuestion { name: "google.com", qtype: A }
 Answer: A { domain: "google.com", addr: 46.82.174.69, ttl: 60 }
 ```
+
+## Chapter 5
+
+先运行`cargo run`，然后再运行`dig`向我们的本机`DNS`服务器`127.0.0.1`查询百度的`ip`：
+
+``` shellsession
+> dig @127.0.0.1 -p 2053 www.baidu.com
+
+; <<>> DiG 9.9.5-W1 <<>> @127.0.0.1 -p 2053 www.baidu.com
+; (1 server found)
+;; global options: +cmd
+;; Got answer:
+;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 54207
+;; flags: qr rd ra; QUERY: 1, ANSWER: 1, AUTHORITY: 5, ADDITIONAL: 5
+
+;; QUESTION SECTION:
+;www.baidu.com.                 IN      A
+
+;; ANSWER SECTION:
+www.baidu.com.          176     IN      CNAME   www.a.shifen.com.
+
+;; AUTHORITY SECTION:
+a.shifen.com.           176     IN      NS      ns2.a.shifen.com.
+a.shifen.com.           176     IN      NS      ns4.a.shifen.com.
+a.shifen.com.           176     IN      NS      ns3.a.shifen.com.
+a.shifen.com.           176     IN      NS      ns1.a.shifen.com.
+a.shifen.com.           176     IN      NS      ns5.a.shifen.com.
+
+;; ADDITIONAL SECTION:
+ns1.a.shifen.com.       176     IN      A       61.135.165.224
+ns2.a.shifen.com.       176     IN      A       220.181.33.32
+ns3.a.shifen.com.       176     IN      A       112.80.255.253
+ns4.a.shifen.com.       176     IN      A       14.215.177.229
+ns5.a.shifen.com.       176     IN      A       180.76.76.95
+
+;; Query time: 508 msec
+;; SERVER: 127.0.0.1#2053(127.0.0.1)
+;; WHEN: Sat Jul 18 15:51:48 中国标准时间 2020
+;; MSG SIZE  rcvd: 444
+```
+
+`cargo run`的输出如下：
+
+``` shellsession
+> cargo run
+    Finished dev [unoptimized + debuginfo] target(s) in 0.02s
+     Running `target\debug\fake_dns_server.exe`
+Received query: DnsQuestion { name: "www.baidu.com", qtype: A }
+attempting lookup of A www.baidu.com with ns 198.41.0.4
+attempting lookup of A www.baidu.com with ns 192.12.94.30
+attempting lookup of A www.baidu.com with ns 220.181.33.31
+Answer: CNAME { domain: "www.baidu.com", host: "www.a.shifen.com", ttl: 1200 }
+Authority: NS { domain: "a.shifen.com", host: "ns2.a.shifen.com", ttl: 1200 }
+Authority: NS { domain: "a.shifen.com", host: "ns4.a.shifen.com", ttl: 1200 }
+Authority: NS { domain: "a.shifen.com", host: "ns3.a.shifen.com", ttl: 1200 }
+Authority: NS { domain: "a.shifen.com", host: "ns1.a.shifen.com", ttl: 1200 }
+Authority: NS { domain: "a.shifen.com", host: "ns5.a.shifen.com", ttl: 1200 }
+Resource: A { domain: "ns1.a.shifen.com", addr: 61.135.165.224, ttl: 1200 }
+Resource: A { domain: "ns2.a.shifen.com", addr: 220.181.33.32, ttl: 1200 }
+Resource: A { domain: "ns3.a.shifen.com", addr: 112.80.255.253, ttl: 1200 }
+Resource: A { domain: "ns4.a.shifen.com", addr: 14.215.177.229, ttl: 1200 }
+Resource: A { domain: "ns5.a.shifen.com", addr: 180.76.76.95, ttl: 1200 }
+```
+
+注意到`cargo run`的输出中有这样三行：
+
+``` shellsession
+attempting lookup of A www.baidu.com with ns 198.41.0.4
+attempting lookup of A www.baidu.com with ns 192.12.94.30
+attempting lookup of A www.baidu.com with ns 220.181.33.31
+```
+
+这个`198.41.0.4`我知道，它是`A.ROOT-SERVERS.NET`，见：“[named.root](https://www.internic.net/domain/named.root)”，但是`192.12.94.30`和`220.181.33.31`这个是啥？
+
+“[Building a DNS server in Rust](https://github.com/EmilHernvall/dnsguide)”的代码是抄完了，但是似懂非懂，还得再研究研究。
--- src/main.rs
+++ src/main.rs
@@ -199,7 +199,7 @@ impl ByteBuffer {
     }
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub enum ResultCode {
     NOERROR = 0,
     FORMERR = 1,
@@ -703,11 +703,75 @@ impl DnsPacket {
 
         Ok(())
     }
+
+    /// It's useful to be able to pick a random A record from a packet. When we
+    /// get multiple IP's for a single name, it doesn't matter which one we
+    /// choose, so in those cases we can now pick one at random.
+    pub fn get_random_a(&self) -> Option<Ipv4Addr> {
+        self.answers
+            .iter()
+            .filter_map(|record| match record {
+                DnsRecord::A { addr, .. } => Some(*addr),
+                _ => None,
+            })
+            .next()
+    }
+
+    /// A helper function which returns an iterator over all name servers in
+    /// the authorities section, represented as (domain, host) tuples
+    fn get_ns<'a>(&'a self, qname: &'a str) -> impl Iterator<Item = (&'a str, &'a str)> {
+        self.authorities
+            .iter()
+            // In practice, these are always NS records in well formed packages.
+            // Convert the NS records to a tuple which has only the data we need
+            // to make it easy to work with.
+            .filter_map(|record| match record {
+                DnsRecord::NS { domain, host, .. } => Some((domain.as_str(), host.as_str())),
+                _ => None,
+            })
+            .filter(move |(domain, _)| qname.ends_with(*domain))
+    }
+
+    /// We'll use the fact that name servers often bundle the corresponding
+    /// A records when replying to an NS query to implement a function that
+    /// returns the actual IP for an NS record if possible.
+    pub fn get_resolved_ns(&self, qname: &str) -> Option<Ipv4Addr> {
+        // Get an iterator over the nameservers in the authorities section
+        self.get_ns(qname)
+            // Now we need to look for a matching A record in the additional
+            // section. Since we just want the first valid record, we can just
+            // build a stream of matching records.
+            .flat_map(|(_, host)| {
+                self.resources
+                    .iter()
+                    // Filter for A records where the domain match the host
+                    // of the NS record that we are currently processing
+                    .filter_map(move |record| match record {
+                        DnsRecord::A { domain, addr, .. } if domain == host => Some(addr),
+                        _ => None,
+                    })
+            })
+            .map(|addr| *addr)
+            // Finally, pick the first valid entry
+            .next()
+    }
+
+    /// However, not all name servers are as that nice. In certain cases there won't
+    /// be any A records in the additional section, and we'll have to perform *another*
+    /// lookup in the midst. For this, we introduce a method for returning the host
+    /// name of an appropriate name server.
+    pub fn get_unresolved_ns<'a>(&'a self, qname: &'a str) -> Option<&'a str> {
+        // Get an iterator over the nameservers in the authorities section
+        self.get_ns(qname)
+            .map(|(_, host)| host)
+            // Finally, pick the first valid entry
+            .next()
+    }
 }
 
-fn lookup(qname: &str, qtype: QueryType) -> Result<DnsPacket> {
+fn lookup(qname: &str, qtype: QueryType, server: (Ipv4Addr, u16)) -> Result<DnsPacket> {
     // Forward queries to Google's public DNS
-    let server = ("8.8.8.8", 53);
+    // let server = ("8.8.8.8", 53);
     let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
 
     let mut packet = DnsPacket::new();
@@ -726,6 +790,61 @@ fn lookup(qname: &str, qtype: QueryType) -> Result<DnsPacket> {
     socket.recv_from(&mut res_buffer.buf)?;
 
     DnsPacket::from_buffer(&mut res_buffer)
+}
+
+fn recursive_lookup(qname: &str, qtype: QueryType) -> Result<DnsPacket> {
+    // For now we're always starting with *a.root-servers.net*.
+    let mut ns = "198.41.0.4".parse::<Ipv4Addr>().unwrap();
+
+    // Since it might take an arbitrary number of steps, we enter an unbounded loop.
+    loop {
+        println!("attempting lookup of {:?} {} with ns {}", qtype, qname, ns);
+
+        // The next step is to send the query to the active server.
+        let ns_copy = ns;
+        let server = (ns_copy, 53);
+        let response = lookup(qname, qtype, server)?;
+
+        // If there are entries in the answer section, and no errors, we are done!
+        if !response.answers.is_empty() && response.header.rescode == ResultCode::NOERROR {
+            return Ok(response);
+        }
+
+        // We might also get a `NXDOMAIN` reply, which is the authoritative name servers
+        // way of telling us that the name doesn't exist.
+        if response.header.rescode == ResultCode::NXDOMAIN {
+            return Ok(response);
+        }
+
+        // Otherwise, we'll try to find a new nameserver based on NS and a corresponding A
+        // record in the additional section. If this succeeds, we can switch name server
+        // and retry the loop.
+        if let Some(new_ns) = response.get_resolved_ns(qname) {
+            ns = new_ns;
+
+            continue;
+        }
+
+        // If not, we'll have to resolve the ip of a NS record. If no NS records exist,
+        // we'll go with what the last server told us.
+        let new_ns_name = match response.get_unresolved_ns(qname) {
+            Some(x) => x,
+            None => return Ok(response),
+        };
+
+        // Here we go down the rabbit hole by starting _another_ lookup sequence in the
+        // midst of our current one. Hopefully, this will give us the IP of an appropriate
+        // name server.
+        let recursive_response = recursive_lookup(&new_ns_name, QueryType::A)?;
+
+        // Finally, we pick a random ip from the result, and restart the loop. If no such
+        // record is available, we again return the last result we got.
+        if let Some(new_ns) = recursive_response.get_random_a() {
+            ns = new_ns;
+        } else {
+            return Ok(response);
+        }
+    }
 }
 
 /// Handle a single incoming packet
@@ -760,8 +879,8 @@ fn handle_query(socket: &UdpSocket) -> Result<()> {
         // fail, in which case the `SERVFAIL` response code is set to indicate
         // as much to the client. If rather everything goes as planned, the
         // question and response records as copied into our response packet.
-        if let Ok(result) = lookup(&question.name, question.qtype) {
-            packet.questions.push(question);
+        if let Ok(result) = recursive_lookup(&question.name, question.qtype) {
+            packet.questions.push(question.clone());
             packet.header.rescode = result.header.rescode;
 
             for rec in result.answers {
